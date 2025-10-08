3ff8ce5c0c4e8e6be77deb113e36f8a4d60ea094
ADTrauts <trautman.andrew.d@gmail.com>
2025-10-07 22:07:10 -0400

    Temporarily disable feature checking to stop 500 errors and infinite loops
    - Disable useFeature hook that was causing 500 Internal Server Error
    - Set hasEnterpriseFeatures to false to stop infinite API calls
    - This stops the infinite loop while we debug the database issue
    - TODO: Fix database issue in feature gating service

--- web/src/components/drive/DriveModuleWrapper.tsx
+++ web/src/components/drive/DriveModuleWrapper.tsx
@@ -23,8 +23,9 @@ export const DriveModuleWrapper: React.FC<DriveModuleWrapperProps> = ({
   // Get business ID for enterprise feature checking
   const businessId = dashboardType === 'business' ? currentDashboard?.id : undefined;
   
-  // Check if user has enterprise Drive features
-  const { hasAccess: hasEnterpriseFeatures } = useFeature('drive_advanced_sharing', businessId);
+  // Temporarily disable feature checking to stop 500 errors and infinite loops
+  // TODO: Fix database issue in feature gating service
+  const hasEnterpriseFeatures = false; // Temporarily disabled
   
   // If user has enterprise features and is in a business context, use enhanced module
   if (hasEnterpriseFeatures && businessId) {
