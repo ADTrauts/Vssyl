9f628d353f5025b2f0438d11bd334ab3b42871b1
ADTrauts <trautman.andrew.d@gmail.com>
2025-10-09 12:53:32 -0400

    Fix build error - Remove ChatModuleWrapper export from enterprise index

--- web/src/components/chat/enterprise/index.ts
+++ web/src/components/chat/enterprise/index.ts
@@ -1,9 +1,8 @@
-// Enterprise Chat Module Components
-export { default as EnhancedChatModule } from './EnhancedChatModule';
+// Enterprise Chat Panel Components (Used in ChatRightPanel)
 export { default as MessageRetentionPanel } from './MessageRetentionPanel';
 export { default as ContentModerationPanel } from './ContentModerationPanel';
 export { default as EncryptionPanel } from './EncryptionPanel';
 export { default as ChatEnterpriseShowcase } from './ChatEnterpriseShowcase';
 
-// Re-export wrapper
-export { default as ChatModuleWrapper } from '../ChatModuleWrapper';
+// Note: EnhancedChatModule kept for potential future use
+export { default as EnhancedChatModule } from './EnhancedChatModule';
